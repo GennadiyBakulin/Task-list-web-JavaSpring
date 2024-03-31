811e14a5e1e4ce4b47881a913ff65e860418d01c
Bakulin Gennadiy <g.bakulin@yandex.ru>
2024-03-31 23:25:19 +0700

    Structure HTML page

---
+++ src/test/java/ru/bakulin/tasklistweb/TaskListWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bakulin.tasklistweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListWebApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
